cdaae26d4bd056b9b044c24c39596d7d47030f70
itsmesaisatish <57112453+itsmesaisatish@users.noreply.github.com>
2021-05-01 17:22:39 +0530

    Update AutomateWhatsAppMessages.py

--- AutomateWhatsAppMessages.py
+++ AutomateWhatsAppMessages.py
@@ -1,2 +1,6 @@
 import pywhatkit as kit
-kit.sendwhatmsg("+9199999999", "Hi ", 18, 38) 
+kit.sendwhatmsg("+9199999999", "Hi ", 18, 38)   
+"""
+In the above code, we have specified the mobile number on which we want to send the message, then the message, and then the time at which the message has to be sent. 
+This module follows the 24 hrs time format, hence the time 18:38 is 06:38 PM.
+"""
